7015fd1eb10c3d290c8e56f8d4e1ff1c3b2d7d16
JaniceWong0410 <wxhjtazjllm@gmail.com>
2026-04-06 21:37:48 +0800

    Update Warrior.java

--- src/sc2002/engine/Warrior.java
+++ src/sc2002/engine/Warrior.java
@@ -2,38 +2,27 @@ package sc2002.engine;
 
 // TODO: Person 2 - Implement player characters
 public class Warrior extends PlayerCombatant {
-    public Warrior() {
-        super("Warrior", PlayerType.WARRIOR, 260, 40, 20, 30);
+    private SpecialSkillAction shieldBash;
+
+    public Warrior(String name) {
+        super(name, Team.WARRIOR, 260, 40, 20, 30);
         this.shieldBash = new WarriorShieldBash();
     }
-    private SpecialSkill shieldBash;
-
-    
-    @Override
-    public void performAction(Combatant target) {
-        basicAttack(target);
-    }
-
-    public void basicAttack(Combatant target) {
-        int damage = Math.max(0, this.attack - target.getDefense());
-        target.takeDamage(damage);
-        System.out.println(this.name + " attacks " + target.getName() + " for " + damage + " damage!");
-    }
-
-    public void defend() {
-        this.defense += 10;
-        System.out.println(this.name + " defends, increasing defense by 10 for this and next round!");
-    }
 
     @Override
-    public void useSpecialSkill(List<Combatant> enemies) {
-        shieldBash.use(this, enemies);
+    public void performAction(List<Combatant> enemies) {
+        Action action = new BasicAttackAction(this, enemies.get(0));
+        action.execute();
     }
 
     @Override
     public void chooseAction(List<Combatant> enemies) {
-        // CLI 输入逻辑，这里简化为默认使用 Shield Bash
         useSpecialSkill(enemies);
+    }
+
+    @Override
+    public void useSpecialSkill(List<Combatant> enemies) {
+        shieldBash.execute(this, enemies);
     }
 
     @Override
