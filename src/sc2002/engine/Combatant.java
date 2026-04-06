253ef79df75fadea32883704b11cec46c078f65f
JaniceWong0410 <wxhjtazjllm@gmail.com>
2026-04-06 20:26:30 +0800

    Update Combatant.java

--- src/sc2002/engine/Combatant.java
+++ src/sc2002/engine/Combatant.java
@@ -1,56 +1,87 @@
 package sc2002.engine;
 
 import java.util.List;
+import java.util.ArrayList;
 
-public interface Combatant {
-    String name();
+public abstract class Combatant {//modified interface to abstract class
+    protected String name;
+    protected int maxHP;
+    protected int currentHP;
+    protected int attack;
+    protected int defense;
+    protected int speed;
+    protected boolean canAct;
 
-    Team team();
+    // combatant can have several effects
+    protected List<Effect> activeEffects;
+    
+    //Constructor
+    public Combatant(String name, int maxHP, int attack, int defense, int speed) {
+        this.name = name;
+        this.maxHP = maxHP;
+        this.currentHP = maxHP;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.canAct = true;
+        this.activeEffects = new ArrayList<>();
+    }
 
-    int getMaxHp();
+    //getters and setters
+    public String getName() { return name; }
+    public int getMaxHP() { return maxHP; }
+    public int getCurrentHP() { return currentHP; }
+    public int getAttack() { return attack; }
+    public int getDefense() { return defense; }
+    public int getSpeed() { return speed; }
+    public boolean canAct() { return canAct; }
+    public void setCanAct(boolean canAct) {
+        this.canAct = canAct;}
+    public int setCurrentHP(int currentHP){return currentHP;}
 
-    int getHp();
+    // basic logic of battle
+    public void takeDamage(int damage) {
+        int newHP = currentHP - damage;
+        currentHP = Math.max(0, newHP); // hp>=0
+        if (currentHP == 0) {
+            System.out.println(name + " has been defeated!");
+        }
+    }
 
-    int getSpeed();
+    public boolean isAlive() {
+        return currentHP > 0;
+    }
 
-    int getAttack();
+    // manage effects
+    public void addEffect(Effect effect) {
+        activeEffects.add(effect);
+        effect.applyEffect(this);
+    }
 
-    int getDefense();
+    public void updateEffects() {
+        List<Effect> expired = new ArrayList<>();
+        for (Effect e : activeEffects) {
+            if (e.isActive()) {
+                e.tick();
+            } else {
+                expired.add(e);
+            }
+        }
+        // remove expired effects
+        for (Effect e : expired) {
+            activeEffects.remove(e);
+        }
+    }
 
-    int getSkillCooldown();
+    // abstract class: different character has different action
+    public abstract void performAction(Combatant target);
 
-    void setSkillCooldown(int turns);
-
-    void tickSkillCooldown();
-
-    void applyDamage(int damage);
-
-    void heal(int amount);
-
-    boolean isAlive();
-
-    void addStatusEffect(StatusEffect effect);
-
-    void applyTurnStartEffects();
-
-    void applyTurnEndEffects();
-
-    List<StatusEffect> getStatusEffects();
-
-    void addAttackBonus(int amount);
-
-    void addDefenseBonus(int amount, int rounds);
-
-    void onRoundEnd();
-
-    boolean consumeActionBlockedForTurn();
-
-    void blockActionForCurrentTurn();
-
-    void consumeItem(String itemName);
-
-    int getItemCount(String itemName);
-
-    void giveItem(String itemName, int count);
+    public String toString() {
+        return name + " [HP: " + currentHP + "/" + maxHP +
+               ", ATK: " + attack +
+               ", DEF: " + defense +
+               ", SPD: " + speed +
+               ", CanAct: " + canAct + "]";
+    }
 }
 
