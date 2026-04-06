96d38285feb7c47278f404a101f67aab04f725a2
JaniceWong0410 <wxhjtazjllm@gmail.com>
2026-04-06 21:46:41 +0800

    Add performAction method to Wolf class

--- src/sc2002/engine/Wolf.java
+++ src/sc2002/engine/Wolf.java
@@ -5,5 +5,12 @@ public class Wolf extends BaseCombatant {
     public Wolf(String name) {
         super(name, Team.ENEMY, 40, 45, 5, 35);
     }
+    public void performAction(List<Combatant> enemies) {
+        if (!enemies.isEmpty()) {
+            Combatant target = enemies.get(0); // 简化为攻击第一个敌人
+            Action action = new BasicAttackAction(this, target);
+            action.execute();
+        }
+    }
 }
 
