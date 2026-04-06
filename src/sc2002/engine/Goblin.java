958789485726792467c1be36b962bd30f398e767
JaniceWong0410 <wxhjtazjllm@gmail.com>
2026-04-06 21:45:48 +0800

    Update Goblin.java

--- src/sc2002/engine/Goblin.java
+++ src/sc2002/engine/Goblin.java
@@ -5,5 +5,13 @@ public class Goblin extends BaseCombatant {
     public Goblin(String name) {
         super(name, Team.ENEMY, 55, 35, 15, 25);
     }
+    @Override
+    public void performAction(List<Combatant> enemies) {
+        if (!enemies.isEmpty()) {
+            Combatant target = enemies.get(0); // 简化为攻击第一个敌人
+            Action action = new BasicAttackAction(this, target);
+            action.execute();
+        }
+    }
 }
 
