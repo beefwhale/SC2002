66eba9340562dff6452ecaca24f10108420252a2
JaniceWong0410 <wxhjtazjllm@gmail.com>
2026-04-06 21:40:49 +0800

    Refactor Wizard class to include arcaneBlast skill

--- src/sc2002/engine/Wizard.java
+++ src/sc2002/engine/Wizard.java
@@ -2,52 +2,31 @@ package sc2002.engine;
 
 // TODO: Person 2 - Implement player characters
 public class Wizard extends PlayerCombatant {
-    public Wizard() {
-        super("Wizard", PlayerType.WIZARD, 200, 50, 10, 20);
-    }
-    @Override
-    public void performAction(Combatant target) {
-        basicAttack(target);
-    }
+    private SpecialSkillAction arcaneBlast;
 
-    public void basicAttack(Combatant target) {
-        int damage = Math.max(0, this.attack - target.getDefense());
-        target.takeDamage(damage);
-        System.out.println(this.name + " attacks " + target.getName() + " for " + damage + " damage!");
-    }
-
-    public void defend() {
-        this.defense += 10;
-        System.out.println(this.name + " defends, increasing defense by 10 for this and next round!");
+    public Wizard(String name) {
+        super(name, Team.WIZARD, 200, 50, 10, 20); 
+        this.arcaneBlast = new WizardArcaneBlast(); 
     }
 
     @Override
-    public void useSpecialSkill(List<Combatant> enemies) {
-        if (skillCooldown == 0) {
-            System.out.println(this.name + " unleashes Arcane Blast!");
-
-            for (Combatant enemy : enemies) {
-                if (enemy.isAlive()) {
-                    int damage = Math.max(0, this.attack - enemy.getDefense());
-                    enemy.takeDamage(damage);
-                    System.out.println("Arcane Blast hits " + enemy.getName() + " for " + damage + " damage!");
-
-                    if (!enemy.isAlive()) {
-                        this.attack += 10;
-                        System.out.println(this.name + " defeated " + enemy.getName() +
-                                           " and gains +10 Attack! Current ATK: " + this.attack);
-                    }
-                }
-            }
-            skillCooldown = 3;
-        } else {
-            System.out.println("Arcane Blast is on cooldown for " + skillCooldown + " more turns!");
-        }
+    public void performAction(List<Combatant> enemies) {
+        Action action = new BasicAttackAction(this, enemies.get(0));
+        action.execute();
     }
 
     @Override
     public void chooseAction(List<Combatant> enemies) {
         useSpecialSkill(enemies);
     }
-}
 
+    @Override
+    public void useSpecialSkill(List<Combatant> enemies) {
+        arcaneBlast.execute(this, enemies);
+    }
+
+    @Override
+    public void updateCooldown() {
+        arcaneBlast.tick();
+    }
+}
