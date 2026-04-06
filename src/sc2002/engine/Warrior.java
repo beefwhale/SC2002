2cf35973cb3aa79cfa74b50870d2e336b5bf2d00
JaniceWong0410 <wxhjtazjllm@gmail.com>
2026-04-06 20:30:12 +0800

    Update Warrior.java

--- src/sc2002/engine/Warrior.java
+++ src/sc2002/engine/Warrior.java
@@ -4,6 +4,41 @@ package sc2002.engine;
 public class Warrior extends PlayerCombatant {
     public Warrior() {
         super("Warrior", PlayerType.WARRIOR, 260, 40, 20, 30);
+        this.shieldBash = new WarriorShieldBash();
+    }
+    private SpecialSkill shieldBash;
+
+    
+    @Override
+    public void performAction(Combatant target) {
+        basicAttack(target);
+    }
+
+    public void basicAttack(Combatant target) {
+        int damage = Math.max(0, this.attack - target.getDefense());
+        target.takeDamage(damage);
+        System.out.println(this.name + " attacks " + target.getName() + " for " + damage + " damage!");
+    }
+
+    public void defend() {
+        this.defense += 10;
+        System.out.println(this.name + " defends, increasing defense by 10 for this and next round!");
+    }
+
+    @Override
+    public void useSpecialSkill(List<Combatant> enemies) {
+        shieldBash.use(this, enemies);
+    }
+
+    @Override
+    public void chooseAction(List<Combatant> enemies) {
+        // CLI 输入逻辑，这里简化为默认使用 Shield Bash
+        useSpecialSkill(enemies);
+    }
+
+    @Override
+    public void updateCooldown() {
+        shieldBash.tick();
     }
 }
 
