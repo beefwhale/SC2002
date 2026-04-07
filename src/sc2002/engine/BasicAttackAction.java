06fc0e7881a2f654c36572659d84407223f12ad7
yang <yangyuqialex@gmail.com>
2026-04-08 00:31:52 +0800

    Modify execute method to return boolean value
    Change execute method to return boolean indicating success.

--- src/sc2002/engine/BasicAttackAction.java
+++ src/sc2002/engine/BasicAttackAction.java
@@ -8,15 +8,16 @@ public class BasicAttackAction implements Action {
     }
 
     @Override
-    public void execute(BattleState state, Combatant actor, Combatant target) {
+    public boolean execute(BattleState state, Combatant actor, Combatant target) {
         if (target == null || !target.isAlive()) {
-            return;
+            return false;
         }
         int damage = Math.max(0, actor.getAttack() - target.getDefense());
         if (actor.team() == Team.ENEMY && state.isEnemyDamageZeroActive()) {
             damage = 0;
         }
         target.applyDamage(damage);
+        return true;
     }
 }
 
