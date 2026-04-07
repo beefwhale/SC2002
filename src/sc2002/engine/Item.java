055a1077006ed226ed8826828ed8a0d9dbc0293b
yang <yangyuqialex@gmail.com>
2026-04-08 00:24:13 +0800

    Change use method to return boolean

--- src/sc2002/engine/Item.java
+++ src/sc2002/engine/Item.java
@@ -3,6 +3,6 @@ package sc2002.engine;
 public interface Item {
     String name();
 
-    void use(BattleState state, Combatant user, Combatant target);
+    boolean use(BattleState state, Combatant user, Combatant target);
 }
 
