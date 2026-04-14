b3f6ef27197ee64bcc5d1f8e6a34904c9cd08836
JaniceWong0410 <wxhjtazjllm@gmail.com>
2026-04-14 21:03:47 +0800

    Update Goblin.java

--- src/sc2002/engine/Goblin.java
+++ src/sc2002/engine/Goblin.java
@@ -1,16 +1,18 @@
 package sc2002.engine;
 
 // TODO: Person 2 - Implement enemy characters
+import java.util.List;
+
 public class Goblin extends BaseCombatant {
+    public BattleState state;
     public Goblin(String name) {
         super(name, Team.ENEMY, 55, 35, 15, 25);
     }
-    @Override
     public void performAction(List<Combatant> enemies) {
         if (!enemies.isEmpty()) {
             Combatant target = enemies.get(0);
-            Action action = new BasicAttackAction(this, target);
-            action.execute();
+            Action action = new BasicAttackAction();
+            action.execute(state, this, target);
         }
     }
 }
