4a3ac6b0e90ef6d1b68aac14f2d88e809d8e90d8
JaniceWong0410 <wxhjtazjllm@gmail.com>
2026-04-14 21:16:22 +0800

    Update Wolf.java

--- src/sc2002/engine/Wolf.java
+++ src/sc2002/engine/Wolf.java
@@ -2,14 +2,15 @@ package sc2002.engine;
 
 // TODO: Person 2 - Implement enemy characters
 public class Wolf extends BaseCombatant {
+    public BattleState state;
     public Wolf(String name) {
         super(name, Team.ENEMY, 40, 45, 5, 35);
     }
     public void performAction(List<Combatant> enemies) {
         if (!enemies.isEmpty()) {
-            Combatant target = enemies.get(0); // 简化为攻击第一个敌人
-            Action action = new BasicAttackAction(this, target);
-            action.execute();
+            Combatant target = enemies.get(0);
+            Action action = new BasicAttackAction();
+            action.execute(state, this, target);
         }
     }
 }
