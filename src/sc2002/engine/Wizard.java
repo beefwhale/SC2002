c06c0300471f95144d5e96c07ea485adbbec31a3
JaniceWong0410 <wxhjtazjllm@gmail.com>
2026-04-14 21:15:47 +0800

    Update Wizard.java

--- src/sc2002/engine/Wizard.java
+++ src/sc2002/engine/Wizard.java
@@ -2,31 +2,11 @@ package sc2002.engine;
 
 // TODO: Person 2 - Implement player characters
 public class Wizard extends PlayerCombatant {
-    private SpecialSkillAction arcaneBlast;
+    public Wizard() {
+        this("Wizard");
+    }
 
     public Wizard(String name) {
-        super(name, Team.WIZARD, 200, 50, 10, 20); 
-        this.arcaneBlast = new WizardArcaneBlast(); 
-    }
-
-    @Override
-    public void performAction(List<Combatant> enemies) {
-        Action action = new BasicAttackAction(this, enemies.get(0));
-        action.execute();
-    }
-
-    @Override
-    public void chooseAction(List<Combatant> enemies) {
-        useSpecialSkill(enemies);
-    }
-
-    @Override
-    public void useSpecialSkill(List<Combatant> enemies) {
-        arcaneBlast.execute(this, enemies);
-    }
-
-    @Override
-    public void updateCooldown() {
-        arcaneBlast.tick();
+        super(name, PlayerType.WIZARD, 200, 50, 10, 20);
     }
 }
