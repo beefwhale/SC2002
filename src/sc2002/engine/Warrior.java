dfc6b9a9173c7167bf246c8a7eca5cc579f92c24
JaniceWong0410 <wxhjtazjllm@gmail.com>
2026-04-14 21:15:17 +0800

    Update Warrior.java

--- src/sc2002/engine/Warrior.java
+++ src/sc2002/engine/Warrior.java
@@ -2,32 +2,12 @@ package sc2002.engine;
 
 // TODO: Person 2 - Implement player characters
 public class Warrior extends PlayerCombatant {
-    private SpecialSkillAction shieldBash;
+    public Warrior() {
+        this("Warrior");
+    }
 
     public Warrior(String name) {
-        super(name, Team.WARRIOR, 260, 40, 20, 30);
-        this.shieldBash = new WarriorShieldBash();
+        super(name, PlayerType.WARRIOR, 260, 40, 20, 30);
     }
-
-    @Override
-    public void performAction(List<Combatant> enemies) {
-        Action action = new BasicAttackAction(this, enemies.get(0));
-        action.execute();
-    }
-
-    @Override
-    public void chooseAction(List<Combatant> enemies) {
-        useSpecialSkill(enemies);
-    }
-
-    @Override
-    public void useSpecialSkill(List<Combatant> enemies) {
-        shieldBash.execute(this, enemies);
-    }
-
-    @Override
-    public void updateCooldown() {
-        shieldBash.tick();
-    }
+    
 }
-
